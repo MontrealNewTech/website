78ac2998366274fb86f1a9ecca7b9b7782e8c4c9
Kira McLean <kiraemclean@gmail.com>
2016-11-05 15:47:54 -0400

    add cover image upload to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,17 @@
 class Event < ApplicationRecord
+  mount_uploader :cover_image, ImageUploader
+
   belongs_to :location
 
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :start_at
+
+  validate :cover_image_size
+
+  private
+
+  def cover_image_size
+    errors.add(:cover_image, 'cannot be more than 5MB') if cover_image.size > 5.megabytes
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  it { is_expected.to belong_to :location }
+
   it { is_expected.to validate_presence_of :name        }
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :start_at    }
