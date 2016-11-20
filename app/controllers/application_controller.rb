a58126bacf1ca39d208f6ac0eebd5a6c20214982
Kira McLean <kiraemclean@gmail.com>
2016-11-20 11:56:31 -0500

    localization from subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+  end
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module Mtlnewtech
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.load_path += Dir[Rails.root.join('locales', '*.yml')]
+    config.i18n.default_locale = :en
+    config.i18n.enforce_available_locales = true
+    config.i18n.available_locales = :en, :fr
   end
 end
