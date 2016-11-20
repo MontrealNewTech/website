c8c7887bd7fb16bb9ae8d93f37e72464e3785737
Kira McLean <kiraemclean@gmail.com>
2016-11-20 12:14:22 -0500

    locale from param and nice route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
 
+  def default_url_options(options={})
+    { locale: I18n.locale }
+  end
+
   private
 
   def set_locale
-    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
-  end
-
-  def extract_locale_from_subdomain
-    parsed_locale = request.subdomains.first
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
