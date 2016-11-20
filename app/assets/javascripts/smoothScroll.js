696336d59964510af8c393d93b73f4ffc4e0361d
Kira McLean <kiraemclean@gmail.com>
2016-11-19 21:27:45 -0500

    fix smooth scroll to work with logo too

--- app/assets/javascripts/smoothScroll.js
+++ app/assets/javascripts/smoothScroll.js
@@ -5,16 +5,35 @@ export default function smoothScroll() {
         let defaults = {
           selector: 'a.scroll-on-page'
         };
-        if (typeof options == 'string') {
+        if (typeof options === 'string') {
           defaults.selector = options;
         }
 
         options = jQuery.extend(defaults, options);
         return jQuery(options.selector).click(function (e) {
           let jumpobj = jQuery(this);
-          let target = jumpobj.attr('href');
+          if (jumpobj.attr('href') === '/') {
+            var target = "#"
+            var offset = 0
+            if ($('#toggle_menu').is(':visible') && $('.menu-toggle').is(':visible')) {
+              $('#toggle_menu').slideToggle(function() {
+                $('body > header').toggleClass('menu-open')
+              })
+            }
+          }
+          else {
+            var target = jumpobj.attr('href');
+            var offset = jQuery(target).offset().top;
+            if ($('.menu-toggle').is(':visible')) {
+              $('#toggle_menu').slideToggle(function() {
+                $('body' > 'header').toggleClass('menu-open')
+                if ($('#toggle_menu').is(':hidden')) {
+                  $('#toggle_menu').removeAttr('style')
+                }
+              })
+            }
+          }
           let thespeed = 1000;
-          let offset = jQuery(target).offset().top;
           jQuery('html,body').animate({
             scrollTop: offset
           }, thespeed, 'swing');
