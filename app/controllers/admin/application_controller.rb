e7bbbd26f0fbd810086e3f8af86731fba095d000
Kira McLean <kiraemclean@gmail.com>
2016-11-30 23:10:25 -0500

    remove guest location controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,6 +2,8 @@ module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
 
+    private
+
     def authenticate_admin
       redirect_to new_user_session_path unless current_user.try(:role) == 'admin'
     end
