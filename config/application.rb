50791831f8bee1ed06a4fb5b9b1f7baf4d58ba8b
Kira McLean <kiraemclean@gmail.com>
2016-12-04 10:58:22 -0500

    remove request specs and prevent them

--- config/application.rb
+++ config/application.rb
@@ -1,29 +1,33 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Mtlnewtech
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
     I18n.load_path += Dir[Rails.root.join('locales', '*.yml')]
     config.i18n.default_locale = :en
     config.i18n.enforce_available_locales = true
     config.i18n.available_locales = :en, :fr
+
+    config.assets.quiet = true
+
+    config.generators do |generate|
+      generate.test_framework :rspec
+      generate.helper false
+      generate.javascript_engine false
+      generate.stylesheets false
+      generate.request_specs false
+      generate.view_specs false
+    end
+
+    config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
