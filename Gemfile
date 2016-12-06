d3702b8482d58bb8530d7ff86e32ea68e43a5369
Kira McLean <kiramclean@users.noreply.github.com>
2016-12-05 21:40:06 -0500

    Use gem for localization (#31)
    * tiny bit of style for events page
    
    * better localization approach

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem 'sprockets-commoner'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 
+# Internationalization
+gem 'route_translator'
+
 # Image uploading
 gem 'carrierwave'
 gem 'cloudinary'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_locale
-
-  def default_url_options(_options={})
-    { locale: I18n.locale }
-  end
 
   def after_sign_in_path_for(_user)
     admin_events_path
-  end
-
-  private
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
