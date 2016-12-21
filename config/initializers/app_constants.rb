c770d28a04ed4bc9031a13d202e2f4430ce49c3f
Kira McLean <kiramclean@users.noreply.github.com>
2016-12-21 19:23:32 -0400

    Start to style calendar (#34)
    * hotfix to at least make all day events load
    
    * style calendar

---
+++ config/initializers/app_constants.rb
@@ -0,0 +1,5 @@
+APP_CONFIG = {
+  community_cal_id: '4b3vms6i8vm26lvg69d7ncobec@group.calendar.google.com',
+  community_cal_link: 'https://calendar.google.com/calendar/render?cid=4b3vms6i8vm26lvg69d7ncobec@group.calendar.google.com#main_7',
+  community_cal_add_link: 'https://docs.google.com/forms/d/e/1FAIpQLSf_Ix9mmxWIbNkDr3lL_tpHZukvOdXwBYf-6b6-H-NO_oMMqw/viewform?c=0&w=1'
+}
