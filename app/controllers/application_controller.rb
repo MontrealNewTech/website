6bdeb3916ccf80301a2368cbcebd5c01d13bf35d
Kira McLean <kiraemclean@gmail.com>
2016-12-21 11:30:13 -0400

    test application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(_user)
-    admin_events_path
+  def after_sign_in_path_for(user)
+    return admin_events_path if user.admin?
+    events_path
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,10 +1,31 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe ApplicationController do
   describe '#after_sign_in_path_for' do
-    it 'always sends users to the admin_events_path' do
-      user = double 'user'
-      expect(controller.after_sign_in_path_for(user)).to eq admin_events_path
+    context 'user is an admin' do
+      it 'sends the user to admin_events_path' do
+        admin = build_stubbed :user, :admin
+        allow(controller).to receive(:current_user).and_return admin
+        expect(controller.after_sign_in_path_for(admin)).to eq admin_events_path
+      end
+    end
+
+    context 'user is not an admin' do
+      it 'sends the user to the events path' do
+        user = build_stubbed :user
+        allow(controller).to receive(:current_user).and_return user
+        expect(controller.after_sign_in_path_for(user)).to eq events_en_path
+      end
+
+      context 'locale is in french' do
+        it 'sends the user to the french events page' do
+          I18n.locale = :fr
+          user = build_stubbed :user
+          allow(controller).to receive(:current_user).and_return user
+          expect(controller.after_sign_in_path_for(user)).to eq events_fr_path
+        end
+      end
     end
   end
 end
