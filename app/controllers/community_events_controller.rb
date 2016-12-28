79bdaf0e308c88808b92ca20cb76eeb34f5345dd
Kira McLean <kiraemclean@gmail.com>
2016-12-27 21:54:48 -0400

    update event calendar to default to week

--- app/controllers/community_events_controller.rb
+++ app/controllers/community_events_controller.rb
@@ -2,12 +2,16 @@
 class CommunityEventsController < ApplicationController
   def index
     @date = from_param_or_today
-    @events_by_date = GoogleCalendarEventFetcher.new(@date).events_calendar
+    @events_by_date = EventsCalendar.new(community_events_this_week).build_week
   end
 
   private
 
   def from_param_or_today
     params[:date] ? Date.parse(params[:date]) : Date.current
+  end
+
+  def community_events_this_week
+    Event.community(for_dates: @date.all_week(:sunday))
   end
 end
--- spec/controllers/community_events_controller_spec.rb
+++ spec/controllers/community_events_controller_spec.rb
@@ -2,9 +2,13 @@
 require 'rails_helper'
 
 RSpec.describe CommunityEventsController, type: :controller do
-  let(:calendar) { instance_double GoogleCalendarEventFetcher, events_calendar: true }
+  let(:calendar) { instance_double EventsCalendar, build_week: true }
 
-  before { allow(GoogleCalendarEventFetcher).to receive(:new).with(Date.current).and_return calendar }
+  before do
+    events = double 'events'
+    allow(Event).to receive(:community).with(for_dates: an_instance_of(Range)).and_return events
+    allow(EventsCalendar).to receive(:new).with(events).and_return calendar
+  end
 
   describe 'GET #index' do
     subject { get :index, params: { locale: I18n.locale.to_param } }
@@ -17,7 +21,7 @@ RSpec.describe CommunityEventsController, type: :controller do
 
     it 'fetches the events from google' do
       subject
-      expect(calendar).to have_received(:events_calendar)
+      expect(calendar).to have_received(:build_week)
     end
   end
 end
