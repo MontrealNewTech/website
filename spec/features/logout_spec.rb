dd72d7a1eb9d33838bf2e751612affa3133355a6
Kira McLean <kiraemclean@gmail.com>
2016-12-29 22:50:25 -0400

    spec for logging out

---
+++ spec/features/logout_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature 'loggout out after being logged in as an admin' do
+  include Stubs
+
+  scenario 'logging out from the admin sidebar' do
+    stub_eventbrite_events_call
+    stub_eventbrite_venue_call
+
+    admin = create :user, :admin
+    login_as admin
+
+    visit admin_initiatives_path
+
+    click_link t('log_out')
+    expect(page).to have_content 'Signed out successfully'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'logging out from the admin sidebar' do
+    stub_eventbrite_events_call
+    stub_eventbrite_venue_call
+
+    admin = create :user, :admin
+    login_as admin
+
+    visit root_path
+
+    click_link t('log_out')
+    expect(page).to have_content 'Signed out successfully'
+    expect(current_path).to eq root_path
+  end
+end
+
