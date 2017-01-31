55a5a5490bf98101bdf419abe38d60dc48884d48
Kira McLean <kiraemclean@gmail.com>
2017-01-31 08:29:35 -0500

    tweak layout spacing and colors

--- app/assets/javascripts/toggleMenu.js
+++ app/assets/javascripts/toggleMenu.js
@@ -1,9 +1,8 @@
 export default function toggleMenu() {
-  let menuToggle = $('.menu-toggle').unbind()
+  var menuToggle = $('.menu-toggle').unbind()
   $('#toggle_menu').removeClass('show');
 
   menuToggle.on('click', function(e) {
-    console.log("CLICK WORKED!!")
     e.preventDefault()
     $('#toggle_menu').slideToggle(function() {
       if($('#toggle_menu').is(':hidden')) {
