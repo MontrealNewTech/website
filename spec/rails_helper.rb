b7e521ccc34066f7a1f584dc25ba5d9c3acd9350
Kira McLean <kiraemclean@gmail.com>
2017-02-02 20:04:04 -0500

    add rspec its for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'spec_helper'
 require 'capybara/rspec'
+require 'rspec/its'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
