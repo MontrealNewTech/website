712865716dd6d103e8e2196a4d5a3f082ca5b467
Kira McLean <kiraemclean@gmail.com>
2017-02-06 23:52:52 -0500

    redirect to index on account update

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -1,6 +1,19 @@
 module Admin
   class AccountsController < Admin::ApplicationController
-    def udpate
+    def update
+      account = Account.find(params[:id])
+
+      if account.update(account_params)
+        redirect_to admin_accounts_path, notice: "Account was successfully updated."
+      else
+        render :edit, alert: "Accout could not be updated"
+      end
+    end
+
+    private
+
+    def account_params
+      params.require(:account).permit(:admin)
     end
   end
 end
