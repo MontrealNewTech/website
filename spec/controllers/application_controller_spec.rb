387ea115d01a55e925ecf8e13f363ce1723725a4
Kira McLean <kiraemclean@gmail.com>
2017-02-12 17:36:04 -0500

    fix application controller

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ApplicationController do
   describe '#after_sign_in_path_for' do
     context 'user is an admin' do
       it 'sends the user to admin_events_path' do
-        admin = build_stubbed :user, :admin
+        admin = instance_double User, admin?: true
         allow(controller).to receive(:current_user).and_return admin
         expect(controller.after_sign_in_path_for(admin)).to eq admin_initiatives_path
       end
@@ -13,7 +13,7 @@ RSpec.describe ApplicationController do
 
     context 'user is not an admin' do
       it 'sends the user to the events path' do
-        user = build_stubbed :user
+        user = instance_double User, admin?: false
         allow(controller).to receive(:current_user).and_return user
         expect(controller.after_sign_in_path_for(user)).to eq community_events_en_path
       end
@@ -21,7 +21,7 @@ RSpec.describe ApplicationController do
       context 'locale is in french' do
         it 'sends the user to the french events page' do
           I18n.locale = :fr
-          user = build_stubbed :user
+          user = instance_double User, admin?: false
           allow(controller).to receive(:current_user).and_return user
           expect(controller.after_sign_in_path_for(user)).to eq community_events_fr_path
           I18n.locale = :en
