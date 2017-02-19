fea3169fa703b8806e20fc2384474d13d51881d1
Kira McLean <kiraemclean@gmail.com>
2017-02-19 13:41:34 -0500

    remove unused spec

--- spec/routing/initiatives_routing_spec.rb
+++ spec/routing/initiatives_routing_spec.rb
@@ -6,9 +6,5 @@ RSpec.describe InitiativesController, type: :routing do
     it 'routes to #index' do
       expect(get: '/initiatives').to route_to('initiatives#index', locale: 'en')
     end
-
-    it 'routes to #show' do
-      expect(get: '/initiatives/1').to route_to('initiatives#show', locale: 'en', id: '1')
-    end
   end
 end
