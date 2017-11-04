9f7210e097c0483c87c97f29b9ba4b9f9620d67d
Kira McLean <kiraemclean@gmail.com>
2017-11-04 17:55:34 -0400

    fix height not width of logos

--- app/helpers/partnerships_helper.rb
+++ app/helpers/partnerships_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module PartnershipsHelper
   def partner_logo(partner)
-    logo = cl_image_tag partner.logo, width: 350, crop: :scale, class: 'photo'
+    logo = cl_image_tag partner.logo, height: 200, crop: :scale, class: 'photo'
     return logo if partner.external_link.blank?
     link_to(partner.external_link, target: 'blank', rel: 'nofollow', class: 'photo') { logo }
   end
