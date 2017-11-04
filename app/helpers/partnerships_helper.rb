84b7e5334353a2dc9425b7bbce1f01298bef5df1
Kira McLean <kiraemclean@gmail.com>
2017-11-04 18:03:28 -0400

    make community partner logos smaller

--- app/helpers/partnerships_helper.rb
+++ app/helpers/partnerships_helper.rb
@@ -5,4 +5,10 @@ module PartnershipsHelper
     return logo if partner.external_link.blank?
     link_to(partner.external_link, target: 'blank', rel: 'nofollow', class: 'photo') { logo }
   end
+
+   def community_partner_logo(partner)
+    logo = cl_image_tag partner.logo, width: 250, crop: :scale, class: 'photo'
+    return logo if partner.external_link.blank?
+    link_to(partner.external_link, target: 'blank', rel: 'nofollow', class: 'photo') { logo }
+   end
 end
